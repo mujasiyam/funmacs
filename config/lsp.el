54a5a13700d09c3beb025cf9662a77d556ce81a3
Muja Mo <mr.mujadiv@gmail.com>
2022-01-20 04:25:46 +0100

    added python language support && fix which-key-integration function

--- config/lsp.el
+++ config/lsp.el
@@ -4,17 +4,19 @@
   :straight t
   :init
   (setq lsp-keymap-prefix "C-c l")
-  :config
-  (lsp-enable-which-key-integration t)
   :commands
   (lsp lsp-deferred)
   :hook
+  (lsp-mode . lsp-enable-which-key-integration)
   ;; LSP LANGUAGES SUPPORT
-  (c-mode . lsp-deferred)             ;; ENABLE C PROGRAMING
-  (c++-mode . lsp-deferred)           ;; ENABLE C++ PROGRAMING
-  (rustic-mode . lsp-deferred)        ;; ENABLE RUST PROGRAMING
-  (lua-mode . lsp-deferred)           ;; ENABLE LUA PROGRAMING
-  (sh-mode . lsp-deferred)            ;; ENABLE SHELL SCRIPT
+  (c-mode . lsp-deferred)             ; ENABLE C PROGRAMING
+  (c++-mode . lsp-deferred)           ; ENABLE C++ PROGRAMING
+  (rustic-mode . lsp-deferred)        ; ENABLE RUST PROGRAMING
+  (lua-mode . lsp-deferred)           ; ENABLE LUA PROGRAMING
+  (sh-mode . lsp-deferred)            ; ENABLE SHELL SCRIPT
+  (python-mode . (lambda ()
+                   (require 'lsp-pyright)
+                   (lsp-deferred)))
   )
 
 ;; LSP-UI MODULES
@@ -25,6 +27,16 @@
 (use-package rustic)
 ;; LUA LANGUAGE SUPPORT
 (use-package lua-mode)
+
+;; PYTHON LANGUAGE SUPPORT
+(use-package lsp-pyright
+  :straight t)
+;; PYHTON ENV
+(use-package pyvenv
+  :straight t
+  :init
+  (setenv "WORKON_HOME" "~/.pyenv/versions"))
+
 
 ;; MARKDOWN-MODE NOT LSP
 (use-package markdown-mode
@@ -39,8 +51,8 @@
   :after (lsp-mode)
   :functions dap-hydra/nil
   :bind (:map lsp-mode-map
-              ("<f5>" . dap-debug)
-              ("M-<f5>" . dap-hydra))
+	      ("<f5>" . dap-debug)
+	      ("M-<f5>" . dap-hydra))
   :hook ((dap-mode . dap-ui-mode)
 	 (dap-session-created . (lambda (&_rest) (dap-hydra)))
 	 (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
